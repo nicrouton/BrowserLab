3b81d814f68c57aa55dfff1fa52414cd4edadf11
nicrouton <144367460+nicrouton@users.noreply.github.com>
2023-11-08 15:25:43 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -0,0 +1,32 @@
+package edu.temple.basicbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var urlEditText: EditText
+    lateinit var goButton: ImageButton
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        urlEditText = findViewById(R.id.urlEditText)
+        goButton = findViewById(R.id.goButton)
+        webView = findViewById(R.id.webView)
+
+        // Allow your browser to intercept hyperlink clicks
+        webView.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
+    }
+}
